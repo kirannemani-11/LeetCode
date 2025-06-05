59efa1f41e4ecfa952477bde1f0eb5a701968821
kirannemani-11 <72664956+kirannemani-11@users.noreply.github.com>
2025-06-04 21:59:28 -0500

    Time: 69 ms (57.42%), Space: 22.9 MB (33.84%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -3,7 +3,7 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         def findd(x1,y1):
             x2,y2 = 0, 0
-            d = math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+            d = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
             return d
         pq = []
         heapq.heapify(pq)
